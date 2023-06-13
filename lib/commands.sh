567f65f8f11dac4679cfa63f193c907d90bb7f79
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-06-14 00:26:02 +0700

    feat: migrate to template v1.3.1

--- lib/commands.sh
+++ lib/commands.sh
@@ -11,6 +11,8 @@ export ASDF_PLUGIN_APP_REPO="https://github.com/helm/helm"
 export ASDF_PLUGIN_NAME="asdf-helm"
 export ASDF_PLUGIN_REPO="https://github.com/kc-workspace/asdf-helm"
 
+# shellcheck source-path=SCRIPTDIR/common.sh
+source "${ASDF_PLUGIN_PATH:?}/lib/common.sh"
 # shellcheck source-path=SCRIPTDIR/utils.sh
 source "${ASDF_PLUGIN_PATH:?}/lib/utils.sh"
 
